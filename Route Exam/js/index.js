351bcda14aa393942e9fb4d95211bbf0a2012d15
Mohey Ahmed <90425906+MoheyAhmed@users.noreply.github.com>
2025-05-09 17:06:48 +0300

    Add files via upload

---
+++ Route Exam/js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", function () {
+    var scroll = this.scrollY;
+    if (scroll > 500) {
+        document.getElementById("nav-scroll").classList.add("second_nav");
+    } else {
+        document.getElementById("nav-scroll").classList.remove("second_nav");
+    }
+});
+
+
+// window.addEventListener("scroll", function (event) {
+//     var scroll = this.scrollY;
+//     if (scroll > 150) {
+//         this.document.getElementById("up").style.display = "block";
+//     } else {
+//         this.document.getElementById("up").style.display = "none";
+//     }
+// });
